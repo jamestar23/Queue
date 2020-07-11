984fc114020068602ac0e047708e77aa2183d185
James Libetario Estrella <estrella.james23@gmail.com>
2020-07-11 13:05:33 -0400

    Events and Delegates

---
+++ OnlineAttendance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineAttendance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+		}
+    }
+
+    public delegate void AlertHandler(object sender, Attendance e);
+
+    public class Attendance : EventArgs
+    {
+        public string name { get; set; }
+
+		List<string> bannedList = new List<string> { 
+			"Jack",
+			"Steven",
+			"Mathew" };
+
+		public void Attendee(string nameA)
+        {
+            if (bannedList.Contains(nameA))
+            {
+                
+            }
+        }
+        
+    }
+   
+
+
+}
